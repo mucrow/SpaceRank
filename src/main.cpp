3481f8e9912707695742f378bad26268d0b4ffd2
mucrow <120820315+mucrow@users.noreply.github.com>
2015-07-11 15:14:33 -0400

    Start using CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,22 @@
-#include "Game.h"
-
 #include <iostream>
+
+#include "Config.h"
+#include "Game.h"
 
 
 int main()
 {
     std::cout
-        << "SFML version: "
+        << "SFML version:      "
         << SFML_VERSION_MAJOR
         << '.'
         << SFML_VERSION_MINOR
+        << std::endl;
+    std::cout
+        << "SpaceRank version: "
+        << SpaceRank_VERSION_MAJOR
+        << '.'
+        << SpaceRank_VERSION_MINOR
         << std::endl;
     Game game;
     return game.run();
