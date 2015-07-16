cd01ae96919d31faa103c54aac779827515011e5
mucrow <120820315+mucrow@users.noreply.github.com>
2015-07-15 20:01:55 -0400

    Make small refactorings

--- src/PlayState.cpp
+++ src/PlayState.cpp
@@ -14,12 +14,8 @@ PlayState::PlayState(Game &game, MsgMgr *msgMgr)
     : State(game, msgMgr)
     , world( b2Vec2(0.0f, -10.0f) )
 {
-    float32 timeStep = 1.0f / 60.0f;
-    int32 velocityIterations = 8;
-    int32 positionIterations = 3;
-
     shared_ptr<Ship> ship(
-        new Ship(world, sf::Vector2f(100,100), 10, 10, 10, 10, 10, 10 ) );
+        new Ship(world, sf::Vector2f(100, 100), 10, 10, 10, 10, 10, 10) );
     entities.push_back(ship);
 }
 
